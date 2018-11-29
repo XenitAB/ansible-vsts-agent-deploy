0b349b1f6ec5b7bdb8a30a39fce456fca7107121
Simon Gottschlag <simon.gottschlag@xenit.se>
2018-11-29 08:45:15 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# ansible-vsts-agent-deploy
-Ansible deployment of VSTS Agent to CoreOS in Azure using containers.
+# Introduction 
+Deployment of VSTS agent to CoreOS
+
+# Information
+Shouldn't be run as release in VSTS / Azure DevOps, since it creates the agents which release pipeline uses.
+
+# How to run
+ansible-playbook -i hosts-prd site.yml -e "AZURE_CLIENT_ID=<ClientID>" -e AZURE_SECRET='"<Secret>"' -e "AZURE_SUBSCRIPTION_ID=<SubscriptionID>" -e "AZURE_TENANT=<TenantID>" -e "VstsAccount=<VstsAccount>" -e VstsToken='"<VstsToken>"' --flush-cache
---
+++ roles/bootstrap-coreos/files/bootstrap.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#
+# https://github.com/ziozzang/python-on-coreos
+#
+
+# Get Version
+VERSION_STR=`wget http://downloads.activestate.com/ActivePython/releases/ -q -O - | grep "folder.gif" | head -n 1  | sed -n 's/.*href="\([^"]*\).*$/\1/p'`
+
+VERSIONS=${VERSIONS:-"${VERSION_STR}"}
+
+# make directory
+mkdir -p /opt/bin
+cd /opt
+
+# Get File Name from sites
+FNAME=`wget http://downloads.activestate.com/ActivePython/releases/${VERSIONS}/ -q -O - | grep "linux" | grep "tar.gz"  | head -n 1  | sed -n 's/.*href="\([^"]*\).*$/\1/p'`
+
+# Get Download.
+wget http://downloads.activestate.com/ActivePython/releases/${VERSIONS}/${FNAME}
+tar -xzvf ${FNAME}
+
+# Rename
+mv `find ./ActivePython-* -type d | head -n 1` apy && cd apy && ./install.sh -I /opt/python/
+
+rm -rf /opt/ActivePython-*
+rm -rf /opt/apy
+
+ln -s /opt/python/bin/easy_install /opt/bin/easy_install
+ln -s /opt/python/bin/pip /opt/bin/pip
+ln -s /opt/python/bin/python /opt/bin/python
+ln -s /opt/python/bin/virtualenv /opt/bin/virtualenv
+
+touch /opt/bin/.bootstrapped
